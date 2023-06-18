55e2e3b91ad23254f54d2ba435aac6fb2dd6011e
Made Oka Resia Wedamerta <69470640+UP3M@users.noreply.github.com>
2023-06-18 18:58:30 +0800

    Update CMSDetail.svelte

--- src/lib/components/CMSDetail.svelte
+++ src/lib/components/CMSDetail.svelte
@@ -1,6 +1,5 @@
-<script lang="ts">
+<script context="module" lang="ts">
 	import DetailCard from './DetailCard.svelte';
-	import { onMount } from 'svelte';
 	import { onValue, ref } from 'firebase/database';
 	import { db } from '$lib/scripts/firestore';
 	import { Post } from '$lib/models/post';
@@ -10,14 +9,21 @@
 	var postsN = new Array();
 
 	var post_final = new Post('', 0, '', '', false, '', null, '');
-	onMount(() => {
-		onValue(ref(db, '/posts'), (s) => {
+	export async function load (){
+		await onValue(ref(db, '/posts'), (s) => {
 			if (s.exists()) {
-				postsN = Object.values(s.val());
+				postsN.push(...Object.values(s.val()));
 				post_final = postsN[parseInt(id)];
 			}
 		});
-	});
+
+		return {
+			props: {
+				post_final,
+				id
+			}
+		};
+	}
 </script>
 
 <div class="row row-cols-1 row-cols-md-3 g-4">
